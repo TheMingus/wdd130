e7ae4a3c7b582cac5a3117c0ddb54523cda17f91
themingus <magnus.chidester+github@gmail.com>
2026-02-25 12:00:45 -0700

    fix function

--- scripts/movie.js
+++ scripts/movie.js
@@ -1,6 +1,6 @@
 async function loadFirstMovie() {
   const data = await fetch("/movie.json").then(r => r.json());
-  document.getElementById("movie").innerHTML = `<a href="${link}" target="_blank" rel="noopener noreferrer"> Most recently, I watched ${title}.</a>`;
+  document.getElementById("movie").innerHTML = `<a href="${data.link}" target="_blank" rel="noopener noreferrer"> Most recently, I watched ${data.title}.</a>`;
 }
 
 loadFirstMovie();
